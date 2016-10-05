e210257ee5c923756dd7297659be48fcba5d7749
Pedro Guimarães <moisespedro15@gmail.com>
2016-10-05 00:17:26 -0300

    ShowCommand checa por MatrixValue e não RefMatrixValue.
     Adicionada exceção para tipos inválidos de show.

--- src/command/ShowCommand.java
+++ src/command/ShowCommand.java
@@ -10,7 +10,6 @@ public class ShowCommand extends Command {
     }
 
     public void execute() {
-        System.out.println("Operação: Show");
         Value<?> v = (value instanceof Variable ? ((Variable) value).value() : value);
 
         if(v instanceof IntValue){
@@ -19,11 +18,11 @@ public class ShowCommand extends Command {
         } else if(v instanceof StringValue){
             String s = ((StringValue) v).value();
             System.out.println(s);
-        } else if(v instanceof RefMatrixValue){
-            Matrix m = ((RefMatrixValue) v).value();
+        } else if(v instanceof MatrixValue) {
+            Matrix m = ((MatrixValue) v).value();
             m.show();
         } else {
-            // TT_INVALID_TOKEN
+        	throw new UnsupportedOperationException("Tipos invalidos");
         }
     }
 }
