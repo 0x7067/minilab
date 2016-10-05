affc4cbdab94aaa7edde51e8aa8d227d756adc1e
Pedro Guimarães <moisespedro15@gmail.com>
2016-10-05 00:30:34 -0300

    Adicionada exceção para tipos inválidos

--- src/model/NullMatrixValue.java
+++ src/model/NullMatrixValue.java
@@ -4,7 +4,8 @@ package model;
  * Created by ccc on 03/10/16.
  */
 public class NullMatrixValue extends MatrixValue {
-    private Value<?> rows, cols;
+
+	private Value<?> rows, cols;
 
     public NullMatrixValue(Value<?> rows, Value<?> cols, int line) {
         super(line);
@@ -14,15 +15,18 @@ public class NullMatrixValue extends MatrixValue {
 
     @Override
     public Matrix value() {
+    	System.out.println("Value?");
+    	
         Value<?> v1 = (rows instanceof Variable ? (Variable) rows.value() : rows);
         Value<?> v2 = (cols instanceof Variable ? (Variable) cols.value() : cols);
 
-        if(v1 instanceof ConstIntValue && v2 instanceof IntValue) {
-            int r = ((ConstIntValue)v1).value();
-            int c = ((ConstIntValue)v2).value();
+        if(v1 instanceof IntValue && v2 instanceof IntValue) {
+            int r = ((IntValue)v1).value();
+            int c = ((IntValue)v2).value();
             return Matrix.m_null(r, c);
         } else {
-            return null;
+        	// FIXME: Erro de tipos
+        	throw new UnsupportedOperationException();
         }
     }
 
